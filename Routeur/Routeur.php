92eb0b43c158f66f930a9d9b3cbd79e29d947f5e
croby98 <guillaumecrob@gmail.com>
2024-01-06 21:10:19 +0100

    routeur en cours

--- Routeur/Routeur.php
+++ Routeur/Routeur.php
@@ -4,5 +4,25 @@ namespace Router;
 
 class Router
 {
-    
+    private array $routes = [];
+
+    public function __construct(private string $uri)
+    {}
+
+    /* On recupére les routes */
+    public function getRoutes(){
+        return $this->routes;
+    }
+
+    /* On creer la route */
+    public function makeRoute(){
+        
+    }
+
+    /* On résout la route */
+    public function resolve($uri)
+    {
+
+    }
+
 }
--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
 
+use Router\Router;
+
 require_once('./../vendor/autoload.php');
+
+$router = new Router($_REQUEST['URI']);
