1e0d5dbebbe04e41181a2e1f4518ce2cdc4e2c0d
croby98 <guillaumecrob@gmail.com>
2024-01-06 21:56:22 +0100

    modif

--- Routeur/Routeur.php
+++ Routeur/Routeur.php
@@ -9,20 +9,5 @@ class Router
     public function __construct(private string $uri)
     {}
 
-    /* On recupére les routes */
-    public function getRoutes(){
-        return $this->routes;
-    }
-
-    /* On creer la route */
-    public function makeRoute(){
-        
-    }
-
-    /* On résout la route */
-    public function resolve($uri)
-    {
-
-    }
 
 }
